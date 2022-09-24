cdaa93a42b22a5aa230aedd284292a9a2f6d50c4
Krasimir Hristov <102061932+Krasimir-Hristov@users.noreply.github.com>
2022-09-24 18:37:41 +0200

    Create HOME and CREATE pages

---
+++ controllers/trip.js
@@ -0,0 +1,42 @@
+const { isUser } = require('../middleware/guards');
+const { createTrip } = require('../services/trip');
+const mapErrors = require('../util/mappers');
+
+const router = require('express').Router();
+
+
+router.get('/create', isUser(), (req, res) => {
+    res.render('create', { title: 'Create Trip Offer', data: {} });
+});
+
+
+router.post('/create', isUser(), async (req, res) => {
+        const trip = {
+            start: req.body.start,
+            end: req.body.end,
+            date: req.body.date,
+            time: req.body.time,
+            carImg: req.body.carImg,
+            carBrand: req.body.carBrand,
+            seats: req.body.seats,
+            price: req.body.price,
+            description: req.body.description,
+            owner: req.session.user._id
+        };
+
+
+        try {
+            await createTrip(trip);
+
+            res.redirect('/trips');
+        } catch (err) {
+            console.error(err);
+            const errors = mapErrors(err);
+            res.render('create', { title: 'Create Trip Offer', data: trip, errors });
+        }
+    });
+            
+
+
+
+module.exports = router;
